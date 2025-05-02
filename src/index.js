dc40c381c10e14181821fed92a6fbe4e162adc7c
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-05-02 14:20:36 +0300

    capitalizing nav component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./Components/nav";
+import App from "./Components/Nav.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
