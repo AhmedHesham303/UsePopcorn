55ad1ef5c177029caae567790d4bc13f10f89d9a
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-05-09 01:18:51 +0300

    making reusable star rating component

--- src/index.js
+++ src/index.js
@@ -7,10 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    /> */}
   </React.StrictMode>
 );
