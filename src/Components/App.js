6e9d88f7ad63dee64449b134e5a6240ece8e4203
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-17 18:21:23 +0300

    ading watched movies to local storage

--- src/Components/App.js
+++ src/Components/App.js
@@ -69,11 +69,19 @@ export default function App() {
 
   function handelAddWatchMovie(movie) {
     setWatched((watched) => [...watched, movie]);
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handelDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
