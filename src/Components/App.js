a885e2735b04a92bb4049bb98870a849e334fd8f
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-17 09:13:08 +0300

    allow deleting a watched movie from watched list

--- src/Components/App.js
+++ src/Components/App.js
@@ -71,6 +71,10 @@ export default function App() {
     setWatched((watched) => [...watched, movie]);
   }
 
+  function handelDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+  }
+
   useEffect(
     function () {
       async function fetchMovies() {
@@ -129,7 +133,10 @@ export default function App() {
             <>
               <WatchedSummary watched={watched} />
 
-              <WatchedMovieList watched={watched} />
+              <WatchedMovieList
+                watched={watched}
+                onDeleteWatched={handelDeleteWatched}
+              />
             </>
           )}
         </Box>
@@ -365,17 +372,21 @@ function WatchedSummary({ watched }) {
   );
 }
 
-function WatchedMovieList({ watched }) {
+function WatchedMovieList({ watched, onDeleteWatched }) {
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <WatchedMovie movie={movie} key={movie.imdbID} />
+        <WatchedMovie
+          movie={movie}
+          key={movie.imdbID}
+          onDeleteWatched={onDeleteWatched}
+        />
       ))}
     </ul>
   );
 }
 
-function WatchedMovie({ movie }) {
+function WatchedMovie({ movie, onDeleteWatched }) {
   return (
     <li>
       <img src={movie.poster} alt={`${movie.title} poster`} />
@@ -393,6 +404,13 @@ function WatchedMovie({ movie }) {
           <span>⏳</span>
           <span>{movie.runtime} min</span>
         </p>
+
+        <button
+          className="btn-delete"
+          onClick={() => onDeleteWatched(movie.imdbID)}
+        >
+          X
+        </button>
       </div>
     </li>
   );
