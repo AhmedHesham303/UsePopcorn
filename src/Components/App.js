db5e47d7addf5ad016aad3976515dc026dbddebb
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-17 09:03:40 +0300

    editing appearing message if watched

--- src/Components/App.js
+++ src/Components/App.js
@@ -234,6 +234,9 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState(0);
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+  const watchedUserRating = watched.find(
+    (movie) => movie.imdbID === selectedId
+  )?.userRating;
   const {
     Title: title,
     Year: year,
@@ -314,7 +317,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
                   )}
                 </>
               ) : (
-                <p>you rated this movie</p>
+                <p>
+                  you rated this movie {watchedUserRating}
+                  <span>⭐</span>
+                </p>
               )}
             </div>
 
